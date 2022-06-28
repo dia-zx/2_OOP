150d10353d3dbb67b617cf3f390cb18fef841b67
dia_zx <dia_zx@mail.ru>
2022-06-28 22:51:00 +0300

    Основные классы Chess смоделированы

---
+++ Lesson1/Task1/ChessBoard.cs
@@ -0,0 +1,18 @@
+﻿namespace Chess
+{
+    public class ChessBoard
+    {
+        public Player player1 { get; set; }
+        public Player player2 { get; set; }
+        public static void DrawBoard(Player player1, Player player2) { 
+        }
+        public Settings settings { get; set; }
+        public bool Save() { return true; }
+        public bool Load() { return true; }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
