d673d85fb3d34192cee15a48dbd349e3ccd09052
dia_zx <dia_zx@mail.ru>
2022-06-28 22:24:34 +0300

    основные классы описаны

---
+++ Lesson1/Task1/Figure.cs
@@ -0,0 +1,53 @@
+﻿namespace Chess
+{
+    public enum FigureColor : byte{
+        White = 0, Black = 1
+    }
+
+    public enum FigureType : byte
+    {
+        pawn = 0,//пешка
+        rook = 1,//ладья
+        horse = 2,// конь
+        elephant = 3,// слон
+        queen = 4, //ферзь
+        king = 5, // король
+    }
+
+    /// <summary>
+    /// абстрактный клас описывающий фигуру
+    /// </summary>
+    public abstract class Figure
+    {
+        /// <summary>
+        /// цвет фигуры
+        /// </summary>
+        public FigureColor Color { get; set; }
+        /// <summary>
+        /// тип фигуры
+        /// </summary>
+        public FigureType Type { get; set; }
+        /// <summary>
+        /// ценность фигуры (может меняться, например, в зависимости от положения...)
+        /// </summary>
+        /// <returns></returns>
+        public abstract byte GetCost();
+        /// <summary>
+        /// положение фигуры по X
+        /// </summary>
+        public byte X { get; set; }
+        /// <summary>
+        /// положение фигуры по Y
+        /// </summary>
+        public byte Y { get; set; }
+        /// <summary>
+        /// выдает список всех возможных ходов фигуры
+        /// </summary>
+        /// <param name="player1">класс с описанием состояния и фигур игрока 1</param>
+        /// <param name="player2">класс с описанием состояния и фигур игрока 2</param>
+        /// <returns>список возможных состояний игроков с наборами фигур... </returns>
+        public abstract List<(Player player1, Player player2)> GetMooves(Player player1, Player player2);
+    }
+
+
+}
---
+++ Lesson1/Task1/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Chess
+{
+    /// <summary>
+    /// Класс описывающий игрока (человека / компьютера)
+    /// </summary>
+    public class Player
+    {
+        public bool IsHuman { get; set; }
+        public FigureColor Color { set; get; }
+        public int Score { get; set; }
+        public List<Figure> Figures { get; set; } = new List<Figure>();
+        public void Reset()
+        {
+
+        }
+    }
+}
