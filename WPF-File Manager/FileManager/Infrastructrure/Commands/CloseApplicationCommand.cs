ebe0663173f0e926f97232f6c356eef841dcdfd3
dia_zx <dia_zx@mail.ru>
2022-07-27 01:39:31 +0300

    Создание окружения

---
+++ WPF-File Manager/FileManager/Infrastructrure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using FileManager.Infrastructrure.Commands.Base;
+
+namespace FileManager.Infrastructrure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
