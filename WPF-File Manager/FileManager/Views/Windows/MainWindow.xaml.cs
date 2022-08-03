47b99b0e8a9556fe07d685d59507e85194705171
dia_zx <dia_zx@mail.ru>
2022-08-03 06:57:55 +0300

    FlleExecuteCommand Right window added

--- WPF-File Manager/FileManager/Views/Windows/MainWindow.xaml.cs
+++ WPF-File Manager/FileManager/Views/Windows/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace FileManager
 {
@@ -15,28 +16,36 @@ namespace FileManager
     {
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
-        
+
 
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             FileManagerClass.GetInstance().FilePanelLeft.CurDir = new DirectoryInfo(Environment.CurrentDirectory); //new DirectoryInfo("C:\\");
- 
-         }
+
+        }
 
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             FileManagerClass.GetInstance().FilePanelLeft.CurDir = new DirectoryInfo("C:\\");
-            if(LeftPanel.SelectedItems == null) 
+            if (LeftPanel.SelectedItems == null)
                 return;
         }
 
         private void LeftPanel_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            FileManagerClass.GetInstance().FilePanelLeft.FilesSelected = LeftPanel.SelectedItems;
+            if (Equals(sender, LeftPanel))
+                FileManagerClass.GetInstance().FilePanelLeft.FilesSelected = ((DataGrid)sender).SelectedItems;
+            if (Equals(sender, RightPanel))
+                FileManagerClass.GetInstance().FilePanelRight.FilesSelected = ((DataGrid)sender).SelectedItems;
+        }
+
+        private void KeyBinding_Changed(object sender, EventArgs e)
+        {
+
         }
     }
 }
